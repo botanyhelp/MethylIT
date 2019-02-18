51a307ce71d4c067810928b4765db6e8df935d1f
genomaths <robersy@gmail.com>
2019-02-17 19:24:54 -0500

    Add the generalized gamma probability density function (PDF) to the set of functions ggamma

--- R/ggamma.R
+++ R/ggamma.R
@@ -2,12 +2,13 @@
 #' @aliases ggamma
 #' @aliases rggamma
 #' @aliases pggamma
+#' @aliases dggamma
 #'
 #' @title Generalized Gamma distribution
-#' @description Cummulative density function (CDF) and random generation for the
-#'     Generalized Gamma distribution with 3 or 4 parameters: alpha, scale, mu,
-#'     and psi. The function is reduced to GGamma distribution with 3
-#'     parameters by setting mu = 0.
+#' @description Probability density function (PDF), cummulative density function
+#'     (CDF) and random generation for the Generalized Gamma (GG) distribution
+#'     with 3 or 4 parameters: alpha, scale, mu, and psi. The function is
+#'     reduced to GGamma distribution with 3 parameters by setting mu = 0.
 #' @details Details about these function can be found in references 1 to 3. You
 #'     may also see section Note at ?pgamma or ?rgamma. Herein, we are using
 #'     Stacy' s formula (references 2 to 3) with the parametrization given in
@@ -15,6 +16,12 @@
 #'     function, the cumulative distribution function (as given in equation 12,
 #'     page 13 from reference 4) is expressed in terms of the lower incomplete
 #'     gamma function (see ?pgamma).
+#'
+#'     The GG distribution with parameters \eqn{\alpha}, \eqn{\beta} (scale),
+#'     \eqn{\psi}, and \eqn{\mu} has density:
+#'
+#'     \deqn{f(x | \alpha, \beta, \mu, \psi) = \alpha exp(-((x-\mu)/
+#'     \beta)^\alpha) ((x-\mu)/\beta)^(\alpha * \psi - 1)/(\beta Gamma(\psi))}
 #'
 #' @param q numeric vector
 #' @param n number of observations
@@ -29,8 +36,8 @@
 #' @param log.p logical; if TRUE, probabilities/densities p are returned as
 #'     log(p).
 #'
-#' @return probability gamma (3-parameters or 4-parameters) for pggamma or
-#'     random generated values for rggamma.
+#' @return GG PDF values (3-parameters or 4-parameters) for dggamma,
+#'     GG probability for pggamma or GG random generated values for rggamma.
 #'
 #' @references 1. Handbook on  STATISTICAL DISTRIBUTIONS for experimentalists
 #' (p. 73) by Christian Walck. Particle Physics Group Fysikum. University of
@@ -57,19 +64,19 @@
 #'
 #' @importFrom stats pgamma rgamma
 #'
-#' @name rggamma
+#' @name dggamma
 #' @rdname ggamma
 #' @title Generalized Gamma distribution
 #' @description NULL
 #' @details NULL
 #' @export
-rggamma <- function(n, alpha=1, scale=1, psi=1) {
-   if (scale <= 0) stop("'scale' parameter must be positive")
-   if (alpha <= 0) stop("'alpha' parameter must be positive")
-   if (psi <= 0) stop("'psi' parameter must be positive")
-
-   r <- scale * (rgamma(n, psi*alpha) ^ (1/alpha))
-   return(r)
+dggamma <- function(q, alpha=1, scale=1, mu=0, psi=1, log.p=FALSE) {
+  if (scale > 0 && alpha > 0 && psi > 0) {
+    y <- (q - mu)/scale
+    d <- exp(-y^alpha) * alpha * y^(alpha * psi - 1)/(scale * gamma(psi))
+    if (log.p) return(log(d)) else return(d)
+  } else d <- NaN
+  return(d)
 }
 #'
 #' @name pggamma
@@ -80,10 +87,24 @@ rggamma <- function(n, alpha=1, scale=1, psi=1) {
 #' @export
 pggamma <- function(q, alpha=1, scale=1, mu=0,
                     psi=1, lower.tail=TRUE, log.p=FALSE) {
-   if (scale > 0 && alpha > 0 && psi > 0) {
-     y <- ((q - mu) / scale ) ^ alpha
-     p <- pgamma(y, psi, lower.tail = lower.tail, log.p = log.p)
-   } else p <- NaN
-   return(p)
+  if (scale > 0 && alpha > 0 && psi > 0) {
+    y <- ((q - mu) / scale ) ^ alpha
+    p <- pgamma(y, shape=psi, lower.tail = lower.tail, log.p = log.p)
+  } else p <- NaN
+  return(p)
 }
 
+#' @name rggamma
+#' @rdname ggamma
+#' @title Generalized Gamma distribution
+#' @description NULL
+#' @details NULL
+#' @export
+rggamma <- function(n, alpha=1, scale=1, psi=1) {
+  if (scale <= 0) stop("'scale' parameter must be positive")
+  if (alpha <= 0) stop("'alpha' parameter must be positive")
+  if (psi <= 0) stop("'psi' parameter must be positive")
+
+  r <- scale * (rgamma(n, psi*alpha) ^ (1/alpha))
+  return(r)
+}
