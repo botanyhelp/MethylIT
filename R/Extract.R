56e4453adff486a0bc5106395fcee1b8e8c50cf6
genomaths <18340747+genomaths@users.noreply.github.com>
2019-03-15 20:13:06 -0400

    Update function Extract

--- R/Extract.R
+++ R/Extract.R
@@ -1,27 +1,33 @@
 #' @rdname Extract
-#' @name "["
-#' @title S3 overloading of subscript operator to preserve class
+#' @title Subscript operator for objects from "InfDiv" and "pDMP" class
 #' @param x A list-like or vector-like object
-#' @param i,... Index specifying elements to extract or replace. Indices are
-#'     numeric or character vectors. See ?base::\code{\link[base]{Extract}} for
+#' @param i Index specifying elements to extract or replace. Index can be a
+#'     numeric or character vector. See ?base::\code{\link[base]{Extract}} for
 #'     a description of these arguments.
 #' @description \eqn{x[i]} returns an object of the length as \eqn{i} preserving
 #'     the class from the original object \eqn{x}.
 #' @return Same as in ?base::\code{\link[base]{Extract}}, but preserving
 #'     the class from the original object \eqn{x}.
 #' @seealso base::\code{\link[base]{Extract}}
-#' @examples
-#' # Create a list
-#' x <- list(a = 1:10, beta = exp(-3:3), logic = c(TRUE,FALSE,FALSE,TRUE))
-#' class(x) <- "nice"
-#'
-#' # Preserves attributes
-#' x[1:2]
+#' @importFrom S4Vectors extractROWS
+
+#' @name "[.InfDiv"
+#' @rdname Extract
 #' @export
-#'
-"[" <- function(x,i, ...) {
+"[.InfDiv" <- function(x, i) {
   cl <- class(x)
-  x <- base::.subset(x,i)
+  x <- .subset(x, i)
   attr(x, "class") <- cl
   return(x)
 }
+
+#' @name "[.pDMP"
+#' @rdname Extract
+#' @export
+"[.pDMP" <- function(x, i) {
+  cl <- class(x)
+  x <- .subset(x, i)
+  attr(x, "class") <- cl
+  return(x)
+}
+
