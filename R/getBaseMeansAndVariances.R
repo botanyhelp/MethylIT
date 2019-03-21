638fcdf383b9619420c78bc2e507ec4d7bae1008
genomaths <18340747+genomaths@users.noreply.github.com>
2019-03-21 15:44:38 -0400

    Update function getDIMPatGenes with @keywords internal

--- R/getBaseMeansAndVariances.R
+++ R/getBaseMeansAndVariances.R
@@ -8,6 +8,7 @@
 #' @return data.frame with means and variances by row
 #'
 #' @importFrom genefilter rowVars
+#' @keywords internal
 .getBaseMeansAndVariances <- function(counts, sizeFactors ) {
    data.frame(baseMean=rowMeans(t(t(counts)/sizeFactors)),
                baseVar=rowVars(t(t(counts)/sizeFactors)))
