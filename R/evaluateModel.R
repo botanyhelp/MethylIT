f54684315a2ba8367f2dfdb255083c574a2295b6
genomaths <18340747+genomaths@users.noreply.github.com>
2019-03-24 00:06:22 -0400

    Update function evaluateModel

--- R/evaluateModel.R
+++ R/evaluateModel.R
@@ -6,11 +6,15 @@
 #'
 #' @param model glm object
 #' @param test A character string matching one of "Wald" or "LRT". If test =
-#'   "Wald", then the p-value of the Wald test for the coefficient of the
-#'   independent variable (\emph{treatment group}) will be reported p-value. If
-#'   test = "LRT", then the p-value from a likelihood ratio test given by
-#'   \code{\link[stats]{anova}} function from \emph{stats} packages will be the
-#'   reported p-value for the group comparison.
+#'     "Wald", then the p-value of the Wald test for the coefficient of the
+#'     independent variable (\emph{treatment group}) will be reported.
+#'     If test = "LRT", then the p-value from a likelihood ratio test given by
+#'     \code{\link[stats]{anova}} function from \emph{stats} packages will be
+#'     the reported p-value for the group comparison when the best fitted model
+#'     is the negative binomial. As suggested for \code{link[stats]{glm}}, if
+#'     best fitted model is Poisson or quasi-Poisson, then the best test is
+#'     'Chi-squared' or 'F-test', respectively. So, for the sake of simplicity,
+#'     the corresponding suitable test will be applied when test = "LRT".
 #' @return AIC value
 #' @keywords internal
 .evaluateModel <- function(model, test = c("Wald", "LRT")) {
@@ -18,9 +22,21 @@
        cfs <- coef(summary(model))
        log2FC <- cfs[2, 1]
        if (test[1] == "LRT") {
-           anov <- try(suppressWarnings(anova(model, test = "Chisq")),
+           if (grepl("Negative", model$family$family))
+               model$family$family <- "negBin"
+           stat <- model$family$family
+           statist <- switch(stat,
+                              quasipoisson = "F",
+                              poisson = "Chisq",
+                              negBin = "LRT"
+           )
+
+           anov <- try(suppressWarnings(anova(model, test = statist)),
                        silent=TRUE)
-           if (!inherits(anov, "try-error")) coef.pval <- anov$`Pr(>Chi)`[2]
+           if (!inherits(anov, "try-error")) {
+               if (statist != "F") coef.pval <- anov$`Pr(>Chi)`[2]
+               else coef.pval <- anov$`Pr(>F)`[2]
+           }
            else coef.pval <- cfs[2, 4]
        } else coef.pval <- cfs[2, 4]
        if (model$family$family == "quasipoisson") {
