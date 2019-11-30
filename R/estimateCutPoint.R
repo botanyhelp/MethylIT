d8d8c0f76f58ada1db9399b3f64ebaa302e09097
genomaths <18340747+genomaths@users.noreply.github.com>
2019-11-30 13:30:50 -0500

    Update estimateCutPoint (just text alignment)

--- R/estimateCutPoint.R
+++ R/estimateCutPoint.R
@@ -320,8 +320,9 @@ estimateCutPoint <- function(LR, control.names, treatment.names, simple = TRUE,
            predClasses[ predClasses == TRUE ] <- "TT"
            predClasses[ predClasses == FALSE ] <- "CT"
            predClasses <- factor(predClasses, levels = c("CT", "TT"))
-           cf.mat <- try(confusionMatrix(data = predClasses, reference = classes,
-                                         positive="TT"),
+           cf.mat <- try(confusionMatrix(data = predClasses,
+                                       reference = classes,
+                                       positive="TT"),
                          silent = TRUE)
 
            if (inherits(cf.mat, "try-error"))
