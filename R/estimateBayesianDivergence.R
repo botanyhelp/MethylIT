95003a1ddc20ca8f7d74ba3ba38f99e144737dc6
Thomas Maher <twm118@psu.edu>
2020-02-05 12:42:44 -0500

    changed one line in R/estimateBayesianDivergence.R, changed FROM: is(x, "matrix") TO: !is(x, "matrix") so that the effect of the code is the same as the original code shown here: class(x) != "matrix"

--- R/estimateBayesianDivergence.R
+++ R/estimateBayesianDivergence.R
@@ -104,7 +104,7 @@ estimateBayesianDivergence <- function(x, Bayesian=FALSE, JD = FALSE,
        bpparam <- SnowParam(workers = num.cores, type = "SOCK")
    }
    ismatrix <- TRUE
-   if (is(x, "matrix")) {
+   if (!is(x, "matrix")) {
        HDiv <- x
        x <- as.matrix(mcols(x))
        ismatrix <- FALSE
