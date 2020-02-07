5fc8ac6bc8971da6d93b212630ee79a1026be601
Thomas Maher <twm118@psu.edu>
2020-02-07 14:32:01 -0500

    changed line 8 of tests/testthat/test_lapply.R FROM: expect_true(class(lapply(x, mean, keep.attr = TRUE)) == "nice") TO: expect_true(is(lapply(x, mean, keep.attr = TRUE), "nice")) ..to cause this WARNING from BiocCheck to go away: WARNING: Avoid class() == or class() != ; use is() or !is() Found in files: tests/testthat/test_lapply.R (line 8)

--- tests/testthat/test_lapply.R
+++ tests/testthat/test_lapply.R
@@ -5,5 +5,5 @@ context("MethylIT lapply tests")
 test_that("lapply function test", {
   x <- list(a = 1:10, beta = exp(-3:3), logic = c(TRUE,FALSE,FALSE,TRUE))
   class(x) <- "nice"
-  expect_true(class(lapply(x, mean, keep.attr = TRUE)) == "nice")
+  expect_true(is(lapply(x, mean, keep.attr = TRUE), "nice"))
 })
